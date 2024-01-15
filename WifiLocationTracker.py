9bcde60a37b63407177e7f17bee40614a13a6a08
Magnus Erler <magnus.erler@radiometer.dk>
2024-01-15 13:44:41 +0100

    show location on a map

--- WifiLocationTracker.py
+++ WifiLocationTracker.py
@@ -3,6 +3,8 @@ import json
 import os
 from dotenv import load_dotenv
 
+import platform
+
 load_dotenv()  # Load variables from .env file
 api_key = os.environ.get('GOOGLE_API_KEY')
 
@@ -29,13 +31,32 @@ def get_device_location(api_key):
             result = response.json()
             location = result.get('location')
             accuracy = result.get('accuracy')
-            print(f"Latitude: {location['lat']}, Longitude: {location['lng']}, Accuracy: {accuracy} meters")
+            return location['lat'], location['lng'], accuracy
         else:
-            # Print error details for unsuccessful requests
             print(f"Error: {response.status_code}, {response.text}")
 
     except Exception as e:
         print(f"An error occurred: {e}")
 
 if __name__ == "__main__":
-    get_device_location(api_key)
+    lat, log, acc = get_device_location(api_key)
+    print("Latitude:", lat, "Longitude:", log, "Accuracy:", acc, "meters")
+
+    # open a html file in browser to see the location on map
+    with open('location.html', 'w') as f:
+        f.write(f"<h1>Latitude: {lat}</h1>")
+        f.write(f"<h1>Longitude: {log}</h1>")
+        f.write(f"<h1>Accuracy: {acc} meters</h1>")
+        f.write(f"<iframe width='100%' height='500' src='https://maps.google.com/maps?q={lat},{log}&output=embed'></iframe>")
+
+    # Use the appropriate command based on the operating system
+    if platform.system() == 'Darwin':  # macOS
+        os.system('open location.html')
+    elif platform.system() == 'Windows':
+        os.system('start location.html')
+    elif platform.system() == 'Linux':
+        os.system('xdg-open location.html')
+    else:
+        print("Unsupported operating system")
+
+
