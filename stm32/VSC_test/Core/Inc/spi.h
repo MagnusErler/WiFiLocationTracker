c2366d8003d5cc8ad6f072abbde84d620b9c96fc
MagnusErler <magnus.erler@gmail.com>
2024-02-09 14:18:47 +0100

    added error messages in spi transfer

--- stm32/VSC_test/Core/Inc/spi.h
+++ stm32/VSC_test/Core/Inc/spi.h
@@ -34,8 +34,9 @@
  * @brief Status code of HAL operation
  */
 typedef enum lr1110_spi_status_e {
-    LR1110_SPI_STATUS_OK    = 0,  //!< SPI operation executed successfuly
-    LR1110_SPI_STATUS_ERROR = 3,  //!< SPI operation failed
+    LR1110_SPI_STATUS_OK        = 0x00,  //!< SPI operation executed successfuly
+    LR1110_SPI_STATUS_ERROR     = 0x01,  //!< SPI operation failed
+    LR1110_SPI_STATUS_TIMEOUT   = 0xFF,  //!< Timeout occured while waiting for a line state
 } lr1110_spi_status_t;
 
 /*!
@@ -45,6 +46,16 @@ typedef enum {
     LR1110_SPI_GPIO_PIN_STATE_LOW,
     LR1110_SPI_GPIO_PIN_STATE_HIGH,
 } lr1110_spi_gpio_pin_state_t;
+
+/*!
+ * @brief Status reported by the HAL layer
+ */
+typedef enum lr1110_gpio_status_e
+{
+    LR1110_GPIO_STATUS_OK      = 0x00,  //!< Operation terminated successfully
+    LR1110_GPIO_STATUS_ERROR   = 0x01,  //!< Operation terminated with error
+    LR1110_GPIO_STATUS_TIMEOUT = 0xFF,  //!< Timeout occured while waiting for a line state
+} lr1110_gpio_status_t;
 
 /*
  * ============================================================================
@@ -67,5 +78,3 @@ typedef enum {
  * @returns Operation status
  */
 lr1110_spi_status_t lr1110_spi_read( const void* context, const uint8_t* cbuffer, const uint16_t cbuffer_length, uint8_t* rbuffer, const uint16_t rbuffer_length );
-
-void setupSPI();
--- stm32/VSC_test/Core/Src/spi.c
+++ stm32/VSC_test/Core/Src/spi.c
@@ -11,75 +11,113 @@
 
 #include "configuration.h"
 
-#include <stdlib.h>
+#include "lr1110_modem_board.h"     // for HAL_DBG_TRACE_ERROR
+
+lr1110_gpio_status_t waitForGPIOState( gpio_t gpio, GPIO_PinState state, uint32_t timeout_ms ) {
+
+    uint32_t start = hal_rtc_get_time_ms( );
+
+    if( state == GPIO_PIN_RESET ) {
+        while( HAL_GPIO_ReadPin( gpio.port, gpio.pin ) == GPIO_PIN_SET ) {
+            if( ( int32_t )( hal_rtc_get_time_ms() - start ) > ( int32_t ) timeout_ms ) {
+                HAL_DBG_TRACE_ERROR("\r\nTimeout occured while waiting for pin %d on port %d to become LOW\r\n", gpio.pin, gpio.port);
+                return LR1110_GPIO_STATUS_TIMEOUT;
+            }
+        };
+    } else {
+        while( HAL_GPIO_ReadPin( gpio.port, gpio.pin ) == GPIO_PIN_RESET ) {
+            if( ( int32_t )( hal_rtc_get_time_ms() - start ) > ( int32_t ) timeout_ms ) {
+                HAL_DBG_TRACE_ERROR("\r\nTimeout occured while waiting for pin %d on port %d to become HIGH\r\n", gpio.pin, gpio.port);
+                return LR1110_GPIO_STATUS_TIMEOUT;
+            }
+        };
+    }
+    return LR1110_GPIO_STATUS_OK;
+}
+
+lr1110_spi_status_t lr1110_spi_write( SPI_TypeDef* spi, const uint8_t* buffer, uint16_t length, uint32_t timeout_ms ) {
+    for( uint16_t i = 0; i < length; i++ ) {
+
+        uint32_t start = hal_rtc_get_time_ms( );
+
+        while( LL_SPI_IsActiveFlag_TXE( spi ) == 0 ) {
+            if( ( int32_t )( hal_rtc_get_time_ms() - start ) > ( int32_t ) timeout_ms ) {
+                HAL_DBG_TRACE_ERROR("\r\nTimeout occured while waiting for SPI to become ready for transmission\r\n");
+                return LR1110_SPI_STATUS_TIMEOUT;
+            }
+        };
+
+        LL_SPI_TransmitData8( spi, buffer[i] );
+
+        while( LL_SPI_IsActiveFlag_RXNE( spi ) == 0 ) {
+            if( ( int32_t )( hal_rtc_get_time_ms() - start ) > ( int32_t ) timeout_ms ) {
+                HAL_DBG_TRACE_ERROR("\r\nTimeout occured while waiting for SPI to become ready for reception\r\n");
+                return LR1110_SPI_STATUS_TIMEOUT;
+            }
+        };
+
+        LL_SPI_ReceiveData8( spi );
+    }
+    return LR1110_SPI_STATUS_OK;
+}
+
+lr1110_spi_status_t lr1110_spi_read_with_dummy_byte( SPI_TypeDef* spi, uint8_t* buffer, uint16_t length, uint8_t dummy_byte, uint32_t timeout_ms ) {
+    for( uint16_t i = 0; i < length; i++ ) {
+
+        uint32_t start = hal_rtc_get_time_ms( );
+
+        while( LL_SPI_IsActiveFlag_TXE( spi ) == 0 ) {
+            if( ( int32_t )( hal_rtc_get_time_ms() - start ) > ( int32_t ) timeout_ms ) {
+                HAL_DBG_TRACE_ERROR("\r\nTimeout occured while waiting for SPI to become ready for transmission\r\n");
+                return LR1110_SPI_STATUS_TIMEOUT;
+            }
+        };
+
+        LL_SPI_TransmitData8( spi, dummy_byte );
+
+        while( LL_SPI_IsActiveFlag_RXNE( spi ) == 0 ) {
+            if( ( int32_t )( hal_rtc_get_time_ms() - start ) > ( int32_t ) timeout_ms ) {
+                HAL_DBG_TRACE_ERROR("\r\nTimeout occured while waiting for SPI to become ready for reception\r\n");
+                return LR1110_SPI_STATUS_TIMEOUT;
+            }
+        };
+
+        buffer[i] = LL_SPI_ReceiveData8( spi );
+    }
+    return LR1110_SPI_STATUS_OK;
+}
 
 lr1110_spi_status_t lr1110_spi_read( const void* context, const uint8_t* cbuffer, const uint16_t cbuffer_length, uint8_t* rbuffer, const uint16_t rbuffer_length ) {
 
   radio_t* radio_local = ( radio_t* ) context;
   uint8_t  dummy_byte  = 0x00;
 
-  lr1110_gpio_wait_for_state( radio_local->busy, LR1110_SPI_GPIO_PIN_STATE_LOW );
+  if (waitForGPIOState( radio_local->busy, GPIO_PIN_RESET, 1000 ) != LR1110_GPIO_STATUS_OK) {
+    return LR1110_SPI_STATUS_ERROR;
+  }
 
   /* 1st SPI transaction */
-  lr1110_gpio_set_pin_state( radio_local->nss, LR1110_SPI_GPIO_PIN_STATE_LOW );
-  system_spi_write( radio_local->spi, cbuffer, cbuffer_length );
-  lr1110_gpio_set_pin_state( radio_local->nss, LR1110_SPI_GPIO_PIN_STATE_HIGH );
+  HAL_GPIO_WritePin( radio_local->nss.port, radio_local->nss.pin, GPIO_PIN_RESET );
+  if (lr1110_spi_write( radio_local->spi, cbuffer, cbuffer_length, 1000 ) != LR1110_SPI_STATUS_OK) {
+    return LR1110_SPI_STATUS_ERROR;
+  }
+  HAL_GPIO_WritePin( radio_local->nss.port, radio_local->nss.pin, GPIO_PIN_SET );
 
-  system_gpio_wait_for_state( radio_local->busy, LR1110_SPI_GPIO_PIN_STATE_LOW );
+  if (waitForGPIOState( radio_local->busy, GPIO_PIN_RESET, 1000 ) != LR1110_GPIO_STATUS_OK) {
+    return LR1110_SPI_STATUS_ERROR;
+  }
 
   /* 2nd SPI transaction */
-  lr1110_gpio_set_pin_state( radio_local->nss, LR1110_SPI_GPIO_PIN_STATE_LOW );
-  system_spi_write( radio_local->spi, &dummy_byte, 1 );
-  lr1110_spi_read_with_dummy_byte( radio_local->spi, rbuffer, rbuffer_length, LR1110_NOP );
-  lr1110_gpio_set_pin_state( radio_local->nss, LR1110_SPI_GPIO_PIN_STATE_HIGH );
+  HAL_GPIO_WritePin( radio_local->nss.port, radio_local->nss.pin, GPIO_PIN_RESET );
+  if (lr1110_spi_write( radio_local->spi, &dummy_byte, 1, 1000 ) != LR1110_SPI_STATUS_OK) {
+    return LR1110_SPI_STATUS_ERROR;
+  }
+  if (lr1110_spi_read_with_dummy_byte( radio_local->spi, rbuffer, rbuffer_length, LR1110_NOP, 1000 ) != LR1110_SPI_STATUS_OK) {
+    return LR1110_SPI_STATUS_ERROR;
+  }
+
+  HAL_GPIO_WritePin( radio_local->nss.port, radio_local->nss.pin, GPIO_PIN_SET );
 
   return LR1110_SPI_STATUS_OK;
 }
 
-void lr1110_gpio_wait_for_state( gpio_t gpio, lr1110_spi_gpio_pin_state_t state ) {
-  if( state == LR1110_SPI_GPIO_PIN_STATE_LOW )
-  {
-      while( LL_GPIO_IsInputPinSet( gpio.port, gpio.pin ) )
-      {
-      };
-  }
-  else
-  {
-      while( !LL_GPIO_IsInputPinSet( gpio.port, gpio.pin ) )
-      {
-      };
-  }
-}
-
-void lr1110_gpio_set_pin_state( gpio_t gpio, const lr1110_spi_gpio_pin_state_t state )
-{
-    switch( state )
-    {
-    case LR1110_SPI_GPIO_PIN_STATE_LOW:
-        LL_GPIO_ResetOutputPin( gpio.port, gpio.pin );
-        break;
-    case LR1110_SPI_GPIO_PIN_STATE_HIGH:
-        LL_GPIO_SetOutputPin( gpio.port, gpio.pin );
-        break;
-    default:
-        break;
-    }
-}
-
-void lr1110_spi_read_with_dummy_byte( SPI_TypeDef* spi, uint8_t* buffer, uint16_t length, uint8_t dummy_byte )
-{
-    for( uint16_t i = 0; i < length; i++ )
-    {
-        while( LL_SPI_IsActiveFlag_TXE( spi ) == 0 )
-        {
-        };
-
-        LL_SPI_TransmitData8( spi, dummy_byte );
-
-        while( LL_SPI_IsActiveFlag_RXNE( spi ) == 0 )
-        {
-        };
-
-        buffer[i] = LL_SPI_ReceiveData8( spi );
-    }
-}
