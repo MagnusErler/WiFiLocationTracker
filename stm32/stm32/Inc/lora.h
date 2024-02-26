027a7b06ec5bc3c901400d7d25e0eaf2e8e06ede
MagnusErler <magnus.erler@gmail.com>
2024-02-26 23:47:33 +0100

    created function to set lora packet params

--- stm32/stm32/Inc/lora.h
+++ stm32/stm32/Inc/lora.h
@@ -4,6 +4,7 @@
 #define LR1110_LORA_CMD_LENGTH_NO_PARAM                 2
 #define LR1110_LORA_CMD_LENGTH_SET_PACKET_TYPE          LR1110_LORA_CMD_LENGTH_NO_PARAM + 1
 #define LR1110_LORA_CMD_LENGTH_SET_MODULATION_PARAMS    LR1110_LORA_CMD_LENGTH_NO_PARAM + 4
+#define LR1110_LORA_CMD_LENGTH_SET_PACKET_PARAMS        LR1110_LORA_CMD_LENGTH_NO_PARAM + 6
 
 // LENGTHS FOR RESPONSES
 #define LR1110_LORA_LENGTH_NO_PARAM                     1
@@ -11,6 +12,7 @@
 // LR1110 WIFI COMMANDS
 #define LR1110_LORA_CMD_SET_PACKET_TYPE                 0x020E
 #define LR1110_LORA_CMD_SET_MODULATION_PARAMS           0x020F
+#define LR1110_LORA_CMD_SET_PACKET_PARAMS               0x0210
 
 /*!
  * @brief Set the LoRa packet type
@@ -24,5 +26,21 @@ void setLR1110_LoRa_Packet_Type( const void* context, uint8_t packet_type  );
  * @brief Set the LoRa modulation parameters
  *
  * @param [in] context Radio abstraction
+ * @param [in] sf Spreading factor
+ * @param [in] bwl Bandwidth
+ * @param [in] cr Coding rate
+ * @param [in] low_data_rate_ptimize Low data rate optimize
  */
 void setLR1110_LoRa_Modulation_Params( const void* context, uint8_t sf, uint8_t bwl, uint8_t cr, uint8_t low_data_rate_ptimize);
+
+/*!
+ * @brief Set the LoRa packet parameters
+ *
+ * @param [in] context Radio abstraction
+ * @param [in] pb_lenght_tx1 Payload length for TX
+ * @param [in] pb_lenght_rx2 Payload length for RX
+ * @param [in] header_type Header type
+ * @param [in] crc CRC
+ * @param [in] invert_iq Invert IQ
+ */
+void setLR1110_LoRa_Packet_Params( const void* context, uint8_t pb_lenght_tx1, uint8_t pb_lenght_tx2, uint8_t header_type, uint8_t crc, uint8_t invert_iq)
--- stm32/stm32/Src/lora.c
+++ stm32/stm32/Src/lora.c
@@ -45,3 +45,23 @@ void setLR1110_LoRa_Modulation_Params( const void* context, uint8_t sf, uint8_t 
         HAL_DBG_TRACE_ERROR("Failed to set LoRa modulation parameters\r\n");
     }
 }
+
+void setLR1110_LoRa_Packet_Params( const void* context, uint8_t pb_lenght_tx1, uint8_t pb_lenght_tx2, uint8_t header_type, uint8_t crc, uint8_t invert_iq) {
+    HAL_DBG_TRACE_INFO("Setting LoRa packet parameters... ");
+
+    uint8_t cbuffer[LR1110_LORA_CMD_LENGTH_SET_PACKET_PARAMS];
+
+    cbuffer[0] = ( uint8_t )( LR1110_LORA_CMD_SET_PACKET_PARAMS >> 8 );
+    cbuffer[1] = ( uint8_t )( LR1110_LORA_CMD_SET_PACKET_PARAMS >> 0 );
+    cbuffer[2] = pb_lenght_tx1;
+    cbuffer[3] = pb_lenght_tx2;
+    cbuffer[4] = header_type;
+    cbuffer[5] = crc;
+    cbuffer[6] = invert_iq;
+
+    if ( lr1110_spi_write( context, cbuffer, LR1110_LORA_CMD_LENGTH_SET_PACKET_PARAMS ) == LR1110_SPI_STATUS_OK ) {
+        HAL_DBG_TRACE_MSG_COLOR("DONE\r\n", HAL_DBG_TRACE_COLOR_GREEN);
+    } else {
+        HAL_DBG_TRACE_ERROR("Failed to set LoRa packet parameters\r\n");
+    }
+}
