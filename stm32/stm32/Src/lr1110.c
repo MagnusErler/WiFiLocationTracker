3ca895ac3461fad68995fa2fcd84aecf908a01b5
MagnusErler <magnus.erler@gmail.com>
2024-03-13 12:44:52 +0100

    clean-up

--- stm32/stm32/Src/lr1110.c
+++ stm32/stm32/Src/lr1110.c
@@ -17,8 +17,8 @@ void getLR1110_Bootloader_Version( const void* context ) {
   uint8_t cbuffer[LR1110_CMD_LENGTH_GET_VERSION];
   uint8_t rbuffer[LR1110_RES_LENGTH_GET_VERSION] = { 0 };
 
-  cbuffer[0] = ( uint8_t )( LR1110_GET_VERSION_CMD >> 8 );
-  cbuffer[1] = ( uint8_t )( LR1110_GET_VERSION_CMD >> 0 );
+  cbuffer[0] = ( uint8_t )( LR1110_CMD_GET_VERSION >> 8 );
+  cbuffer[1] = ( uint8_t )( LR1110_CMD_GET_VERSION >> 0 );
   
   if (lr1110_spi_read(context, cbuffer, LR1110_CMD_LENGTH_GET_VERSION, rbuffer, LR1110_RES_LENGTH_GET_VERSION ) == LR1110_SPI_STATUS_OK) {
     HAL_DBG_TRACE_INFO_VALUE("HW: %d (0x%X), ", rbuffer[1], rbuffer[1]);
@@ -45,8 +45,8 @@ float getLR1110_Temperature( const void* context ) {
   uint8_t cbuffer[LR1110_CMD_LENGTH_GET_TEMPERATURE];
   uint8_t rbuffer[LR1110_RES_LENGTH_GET_TEMPERATURE] = { 0 };
 
-  cbuffer[0] = ( uint8_t )( LR1110_GET_TEMPERATURE_CMD >> 8 );
-  cbuffer[1] = ( uint8_t )( LR1110_GET_TEMPERATURE_CMD >> 0 );
+  cbuffer[0] = ( uint8_t )( LR1110_CMD_GET_TEMPERATURE >> 8 );
+  cbuffer[1] = ( uint8_t )( LR1110_CMD_GET_TEMPERATURE >> 0 );
 
   if (lr1110_spi_read( context, cbuffer, LR1110_CMD_LENGTH_GET_TEMPERATURE, rbuffer, LR1110_RES_LENGTH_GET_TEMPERATURE ) == LR1110_SPI_STATUS_OK) {
     uint16_t temp_10_0 = ((rbuffer[1] << 8) | rbuffer[2]) & 0x7FF;
@@ -69,8 +69,8 @@ void getLR1110_ChipEUI( const void* context ) {
   uint8_t cbuffer[LR1110_CMD_LENGTH_GET_CHIPEUI];
   uint8_t rbuffer[LR1110_RES_LENGTH_GET_CHIPEUI] = { 0 };
 
-  cbuffer[0] = ( uint8_t )( LR1110_GET_CHIPEUI_CMD >> 8 );
-  cbuffer[1] = ( uint8_t )( LR1110_GET_CHIPEUI_CMD >> 0 );
+  cbuffer[0] = ( uint8_t )( LR1110_CMD_GET_CHIPEUI >> 8 );
+  cbuffer[1] = ( uint8_t )( LR1110_CMD_GET_CHIPEUI >> 0 );
 
   if (lr1110_spi_read( context, cbuffer, LR1110_CMD_LENGTH_GET_CHIPEUI, rbuffer, LR1110_RES_LENGTH_GET_CHIPEUI ) == LR1110_SPI_STATUS_OK) {
     HAL_DBG_TRACE_INFO_VALUE("%02X:%02X:%02X:%02X:%02X:%02X:%02X:%02X\r\n", rbuffer[1], rbuffer[2], rbuffer[3], rbuffer[4], rbuffer[5], rbuffer[6], rbuffer[7], rbuffer[8]);
@@ -79,14 +79,14 @@ void getLR1110_ChipEUI( const void* context ) {
   }
 }
 
-void getLR1110_Semtech_JoinEui( const void* context ) {
+void getLR1110_Semtech_JoinEui( const void* context, uint8_t* join_eui  ) {
   HAL_DBG_TRACE_INFO("Getting LR1110 Semtech JoinEUI... ");
 
   uint8_t cbuffer[LR1110_CMD_LENGTH_GET_SEMTECH_JOINEUI];
   uint8_t rbuffer[LR1110_RES_LENGTH_GET_SEMTECH_JOINEUI] = { 0 };
 
-  cbuffer[0] = ( uint8_t )( LR1110_GET_SEMTECH_JOINEUI_CMD >> 8 );
-  cbuffer[1] = ( uint8_t )( LR1110_GET_SEMTECH_JOINEUI_CMD >> 0 );
+  cbuffer[0] = ( uint8_t )( LR1110_CMD_GET_SEMTECH_JOINEUI >> 8 );
+  cbuffer[1] = ( uint8_t )( LR1110_CMD_GET_SEMTECH_JOINEUI >> 0 );
 
   if (lr1110_spi_read( context, cbuffer, LR1110_CMD_LENGTH_GET_SEMTECH_JOINEUI, rbuffer, LR1110_RES_LENGTH_GET_SEMTECH_JOINEUI ) == LR1110_SPI_STATUS_OK) {
     HAL_DBG_TRACE_INFO_VALUE("%02X:%02X:%02X:%02X:%02X:%02X:%02X:%02X\r\n", rbuffer[1], rbuffer[2], rbuffer[3], rbuffer[4], rbuffer[5], rbuffer[6], rbuffer[7], rbuffer[8]);
@@ -101,8 +101,8 @@ float getLR1110_Battery_Voltage( const void* context ) {
   uint8_t cbuffer[LR1110_CMD_LENGTH_GET_BATTERY_VOLTAGE];
   uint8_t rbuffer[LR1110_RES_LENGTH_GET_BATTERY_VOLTAGE] = { 0 };
 
-  cbuffer[0] = ( uint8_t )( LR1110_GET_BATTERY_VOLTAGE_CMD >> 8 );
-  cbuffer[1] = ( uint8_t )( LR1110_GET_BATTERY_VOLTAGE_CMD >> 0 );
+  cbuffer[0] = ( uint8_t )( LR1110_CMD_GET_BATTERY_VOLTAGE >> 8 );
+  cbuffer[1] = ( uint8_t )( LR1110_CMD_GET_BATTERY_VOLTAGE >> 0 );
 
   if (lr1110_spi_read( context, cbuffer, LR1110_CMD_LENGTH_GET_BATTERY_VOLTAGE, rbuffer, LR1110_RES_LENGTH_GET_BATTERY_VOLTAGE ) == LR1110_SPI_STATUS_OK) {
     const float batteryVoltage = (((5 * rbuffer[1])/255.0) - 1) * 1.35;
@@ -119,8 +119,8 @@ void setLR1110_TCXO_Mode( const void* context ) {
 
   uint8_t cbuffer[LR1110_CMD_LENGTH_SET_TCXO_MODE];
 
-  cbuffer[0] = ( uint8_t )( LR1110_SET_TCXO_MODE_CMD >> 8 );
-  cbuffer[1] = ( uint8_t )( LR1110_SET_TCXO_MODE_CMD >> 0 );
+  cbuffer[0] = ( uint8_t )( LR1110_CMD_SET_TCXO_MODE >> 8 );
+  cbuffer[1] = ( uint8_t )( LR1110_CMD_SET_TCXO_MODE >> 0 );
 
   const uint8_t timeout = ( 5 * 1000 ) / 30.52;  // BOARD_TCXO_WAKEUP_TIME = 5  // 163 (163.826998689)
   cbuffer[2] = ( uint8_t ) 0x02;
@@ -140,8 +140,8 @@ void getLR1110_Status( const void* context ) {
 
   uint8_t cbuffer[LR1110_CMD_LENGTH_GET_STATUS];
 
-  cbuffer[0] = ( uint8_t )( LR1110_GET_STATUS_CMD >> 8 );
-  cbuffer[1] = ( uint8_t )( LR1110_GET_STATUS_CMD >> 0 );
+  cbuffer[0] = ( uint8_t )( LR1110_CMD_GET_STATUS >> 8 );
+  cbuffer[1] = ( uint8_t )( LR1110_CMD_GET_STATUS >> 0 );
   cbuffer[2] = 0x00;
   cbuffer[3] = 0x00;
   cbuffer[4] = 0x00;
@@ -159,8 +159,8 @@ void calibrateLR1110_Image( const void* context, uint8_t freq1, uint8_t freq2) {
 
   uint8_t cbuffer[LR1110_CMD_LENGTH_CALIBRATE_IMAGE];
 
-  cbuffer[0] = ( uint8_t )( LR1110_CALIBRATE_IMAGE_CMD >> 8 );
-  cbuffer[1] = ( uint8_t )( LR1110_CALIBRATE_IMAGE_CMD >> 0 );
+  cbuffer[0] = ( uint8_t )( LR1110_CMD_CALIBRATE_IMAGE >> 8 );
+  cbuffer[1] = ( uint8_t )( LR1110_CMD_CALIBRATE_IMAGE >> 0 );
   cbuffer[2] = freq1;
   cbuffer[3] = freq2;
 
@@ -176,8 +176,8 @@ void calibrateLR1110( const void* context, uint8_t calib_params) {
 
   uint8_t cbuffer[LR1110_CMD_LENGTH_CALIBRATE];
 
-  cbuffer[0] = ( uint8_t )( LR1110_CALIBRATE_CMD >> 8 );
-  cbuffer[1] = ( uint8_t )( LR1110_CALIBRATE_CMD >> 0 );
+  cbuffer[0] = ( uint8_t )( LR1110_CMD_CALIBRATE >> 8 );
+  cbuffer[1] = ( uint8_t )( LR1110_CMD_CALIBRATE >> 0 );
   cbuffer[2] = calib_params;
 
   if (lr1110_spi_write( context, cbuffer, LR1110_CMD_LENGTH_CALIBRATE ) == LR1110_SPI_STATUS_OK) {
@@ -192,8 +192,8 @@ void clearLR1110_Errors( const void* context) {
 
   uint8_t cbuffer[LR1110_CMD_LENGTH_CLEAR_ERRORS];
 
-  cbuffer[0] = ( uint8_t )( LR1110_CLEAR_ERRORS_CMD >> 8 );
-  cbuffer[1] = ( uint8_t )( LR1110_CLEAR_ERRORS_CMD >> 0 );
+  cbuffer[0] = ( uint8_t )( LR1110_CMD_CLEAR_ERRORS >> 8 );
+  cbuffer[1] = ( uint8_t )( LR1110_CMD_CLEAR_ERRORS >> 0 );
 
   if (lr1110_spi_write( context, cbuffer, LR1110_CMD_LENGTH_CLEAR_ERRORS ) == LR1110_SPI_STATUS_OK) {
     HAL_DBG_TRACE_MSG_COLOR("DONE\r\n", HAL_DBG_TRACE_COLOR_GREEN);
@@ -207,8 +207,8 @@ void clearLR1110_IRQ( const void* context ) {
 
   uint8_t cbuffer[LR1110_CMD_LENGTH_CLEAR_INTERRUPTS];
 
-  cbuffer[0] = ( uint8_t )( LR1110_CLEAR_INTERRUPTS_CMD >> 8 );
-  cbuffer[1] = ( uint8_t )( LR1110_CLEAR_INTERRUPTS_CMD >> 0 );
+  cbuffer[0] = ( uint8_t )( LR1110_CMD_CLEAR_INTERRUPTS >> 8 );
+  cbuffer[1] = ( uint8_t )( LR1110_CMD_CLEAR_INTERRUPTS >> 0 );
   cbuffer[2] = 0b11111111;
   cbuffer[3] = 0b11111111;
   cbuffer[4] = 0b11111111;
@@ -227,8 +227,8 @@ void getLR1110_Errors( const void* context ) {
   uint8_t cbuffer[LR1110_CMD_LENGTH_GET_ERRORS];
   uint8_t rbuffer[LR1110_RES_LENGTH_GET_ERRORS] = { 0 };
 
-  cbuffer[0] = ( uint8_t )( LR1110_GET_ERRORS_CMD >> 8 );
-  cbuffer[1] = ( uint8_t )( LR1110_GET_ERRORS_CMD >> 0 );
+  cbuffer[0] = ( uint8_t )( LR1110_CMD_GET_ERRORS >> 8 );
+  cbuffer[1] = ( uint8_t )( LR1110_CMD_GET_ERRORS >> 0 );
 
   if (lr1110_spi_read( context, cbuffer, LR1110_CMD_LENGTH_GET_ERRORS, rbuffer, LR1110_RES_LENGTH_GET_ERRORS ) == LR1110_SPI_STATUS_OK) {
     // Extracting error status bits
@@ -277,8 +277,8 @@ void resetLR1110( const void* context, const uint8_t reset_type) {
   // REMEMBER THAT BUSY STAYS HIGH WHILE THE CHIP IS REBOOTING
   // uint8_t cbuffer[LR1110_CMD_LENGTH_REBOOT];
 
-  // cbuffer[0] = ( uint8_t )( LR1110_REBOOT_CMD >> 8 );
-  // cbuffer[1] = ( uint8_t )( LR1110_REBOOT_CMD >> 0 );
+  // cbuffer[0] = ( uint8_t )( LR1110_CMD_REBOOT >> 8 );
+  // cbuffer[1] = ( uint8_t )( LR1110_CMD_REBOOT >> 0 );
   // cbuffer[2] = reset_type;
 
   // if (lr1110_spi_write( context, cbuffer, LR1110_CMD_LENGTH_REBOOT, true ) != LR1110_SPI_STATUS_OK) {
@@ -295,8 +295,8 @@ void setLR1110_Dio_Irq_Params( const void* context, const uint64_t irq1_to_enabl
 
   uint8_t cbuffer[LR1110_CMD_LENGTH_SET_DIO_IRQ_PARAMS];
 
-  cbuffer[0] = ( uint8_t )( LR1110_SET_DIO_IRQ_PARAMS_CMD >> 8 );
-  cbuffer[1] = ( uint8_t )( LR1110_SET_DIO_IRQ_PARAMS_CMD >> 0 );
+  cbuffer[0] = ( uint8_t )( LR1110_CMD_SET_DIO_IRQ_PARAMS >> 8 );
+  cbuffer[1] = ( uint8_t )( LR1110_CMD_SET_DIO_IRQ_PARAMS >> 0 );
 
   cbuffer[2] = ( uint8_t )( irq1_to_enable >> 24 );
   cbuffer[3] = ( uint8_t )( irq1_to_enable >> 16 );
@@ -320,8 +320,8 @@ void setLR1110_Standby_Mode( const void* context, const uint8_t stdby_config ) {
 
   uint8_t cbuffer[LR1110_CMD_LENGTH_SET_STANDBY_MODE];
 
-  cbuffer[0] = ( uint8_t )( LR1110_SET_STANDBY_MODE_CMD >> 8 );
-  cbuffer[1] = ( uint8_t )( LR1110_SET_STANDBY_MODE_CMD >> 0 );
+  cbuffer[0] = ( uint8_t )( LR1110_CMD_SET_STANDBY_MODE >> 8 );
+  cbuffer[1] = ( uint8_t )( LR1110_CMD_SET_STANDBY_MODE >> 0 );
   cbuffer[2] = stdby_config;
 
   if (lr1110_spi_write( context, cbuffer, LR1110_CMD_LENGTH_SET_STANDBY_MODE ) == LR1110_SPI_STATUS_OK ) {
