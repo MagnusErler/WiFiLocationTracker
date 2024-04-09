6e42c32eac20700e7284274a8db7be7cd20680ca
MagnusErler <magnus.erler@gmail.com>
2024-04-09 11:29:57 +0200

    moved folder one folde rout

---
+++ stm32/stm32/3_geolocation_on_lora_edge/build_lr1110_l4/ral_lr11xx_bsp.d
@@ -0,0 +1,26 @@
+build_lr1110_l4/ral_lr11xx_bsp.o: radio_hal/ral_lr11xx_bsp.c \
+ ../lbm_lib/smtc_modem_core/smtc_ral/src/ral_lr11xx_bsp.h \
+ ../lbm_lib/smtc_modem_core/smtc_ral/src/ral_defs.h \
+ ../lbm_lib/smtc_modem_core/smtc_ral/src/lr_fhss_v1_base_types.h \
+ ../lbm_lib/smtc_modem_core/radio_drivers/lr11xx_driver/src/lr11xx_radio_types.h \
+ ../lbm_lib/smtc_modem_core/radio_drivers/lr11xx_driver/src/lr11xx_system_types.h \
+ radio_hal/lr11xx_pa_pwr_cfg.h smtc_hal_l4/smtc_hal_mcu.h \
+ smtc_hal_l4/smtc_hal_dbg_trace.h smtc_hal_l4/smtc_hal_trace.h \
+ radio_hal/radio_utilities.h ../lbm_lib/smtc_modem_hal/smtc_modem_hal.h \
+ ../lbm_lib/smtc_modem_core/radio_drivers/lr11xx_driver/src/lr11xx_radio.h \
+ ../lbm_lib/smtc_modem_core/radio_drivers/lr11xx_driver/src/lr11xx_radio_types.h \
+ ../lbm_lib/smtc_modem_core/radio_drivers/lr11xx_driver/src/lr11xx_types.h
+../lbm_lib/smtc_modem_core/smtc_ral/src/ral_lr11xx_bsp.h:
+../lbm_lib/smtc_modem_core/smtc_ral/src/ral_defs.h:
+../lbm_lib/smtc_modem_core/smtc_ral/src/lr_fhss_v1_base_types.h:
+../lbm_lib/smtc_modem_core/radio_drivers/lr11xx_driver/src/lr11xx_radio_types.h:
+../lbm_lib/smtc_modem_core/radio_drivers/lr11xx_driver/src/lr11xx_system_types.h:
+radio_hal/lr11xx_pa_pwr_cfg.h:
+smtc_hal_l4/smtc_hal_mcu.h:
+smtc_hal_l4/smtc_hal_dbg_trace.h:
+smtc_hal_l4/smtc_hal_trace.h:
+radio_hal/radio_utilities.h:
+../lbm_lib/smtc_modem_hal/smtc_modem_hal.h:
+../lbm_lib/smtc_modem_core/radio_drivers/lr11xx_driver/src/lr11xx_radio.h:
+../lbm_lib/smtc_modem_core/radio_drivers/lr11xx_driver/src/lr11xx_radio_types.h:
+../lbm_lib/smtc_modem_core/radio_drivers/lr11xx_driver/src/lr11xx_types.h:
