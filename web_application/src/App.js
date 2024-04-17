864297717e880abadf51860d706fa452be2ef558
promillen <s223573@dtu.dk>
2024-04-17 23:49:25 +0200

    Implementet .env and API call to get and set name of devices

--- web_application/src/App.js
+++ web_application/src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import "./styles.css";
 import "leaflet/dist/leaflet.css";
@@ -21,121 +21,159 @@ export default function App() {
   const [markers, setMarkers] = useState([
       {
         pingId: 1,
-        deviceId: 1,
+        deviceId: "0016c001f0003fc5",
         geocode: [56.86, 10.1522],
         timeStamp: convertUnixSecondsToReadableDate(1712781085),
       },
       {
         pingId: 2,
-        deviceId: 1,
+        deviceId: "0016c001f0003fc5",
         geocode: [56.85, 10.1522],
         timeStamp: convertUnixSecondsToReadableDate(1712761085),
       },
       {
         pingId: 3,
-        deviceId: 1,
+        deviceId: "0016c001f0003fc5",
         geocode: [56.855, 10.14],
         timeStamp: convertUnixSecondsToReadableDate(1712721085),
       },
       {
         pingId: 4,
-        deviceId: 2,
+        deviceId: "0016c001f0003ff5",
         geocode: [56.845, 10.1328],
         timeStamp: convertUnixSecondsToReadableDate(1712681085),
       },
       {
         pingId: 5,
-        deviceId: 2,
+        deviceId: "0016c001f0003ff5",
         geocode: [56.838, 10.129],
         timeStamp: convertUnixSecondsToReadableDate(1712781150),
       },
       {
         pingId: 6,
-        deviceId: 3,
+        deviceId: "0016c001f0005f5d",
         geocode: [55.352129024259426, 10.371932315878896],
         timeStamp: convertUnixSecondsToReadableDate(1712800065),
       },
       {
         pingId: 7,
-        deviceId: 3,
+        deviceId: "0016c001f0005f5d",
         geocode: [55.39036773193939, 10.003890323691396],
         timeStamp: convertUnixSecondsToReadableDate(1712805065),
       },
       {
         pingId: 8,
-        deviceId: 3,
+        deviceId: "0016c001f0005f5d",
         geocode: [55.51186134119964, 9.76631097798827],
         timeStamp: convertUnixSecondsToReadableDate(1712810065),
       },
       {
         pingId: 9,
-        deviceId: 3,
+        deviceId: "0016c001f0005f5d",
         geocode: [55.48385755904464, 9.471053409628896],
         timeStamp: convertUnixSecondsToReadableDate(1712815065),
       },
       {
         pingId: 10,
-        deviceId: 4,
+        deviceId: "0016c001f0005f84",
         geocode: [55.21336109122791, 11.76730468116085],
         timeStamp: convertUnixSecondsToReadableDate(1712800065),
       },
       {
         pingId: 11,
-        deviceId: 4,
+        deviceId: "0016c001f0005f84",
         geocode: [54.97306836214099, 12.002836349452343],
         timeStamp: convertUnixSecondsToReadableDate(1712805065),
       },
       {
         pingId: 12,
-        deviceId: 4,
+        deviceId: "0016c001f0005f84",
         geocode: [54.576789525964145, 11.929138297811294],
         timeStamp: convertUnixSecondsToReadableDate(1712810065),
       },
       {
         pingId: 13,
-        deviceId: 4,
+        deviceId: "0016c001f0005f84",
         geocode: [54.08320371108278, 12.134133963429825],
         timeStamp: convertUnixSecondsToReadableDate(1712815065),
       },
       {
         pingId: 14,
-        deviceId: 4,
+        deviceId: "0016c001f0005f84",
         geocode: [53.853835486983506, 12.444120362979492],
         timeStamp: convertUnixSecondsToReadableDate(1712820065),
       }
   ]);
 
   // TODO: fetch tracker information from API
-  const trackerInformation = [
-    {
-      deviceId: 1,
-      name: "Hilti rundsav",
-      batteryStatus: "100%",
-      temperature: "20°C",
-      updateInterval: "5 min"
-    },
-    {
-      deviceId: 2,
-      name: "MaiCut flamingoskærer",
-      batteryStatus: "100%",
-      temperature: "24°C",
-      updateInterval: "2.5 timer"
-    },
-    {
-      deviceId: 3,
-      name: "SoRoTo tvangsblander 40 liter",
-      batteryStatus: "100%",
-      temperature: "-6°C",
-      updateInterval: "12 timer"
-    },
-    {
-      deviceId: 4,
-      name: "Milwaukee Inspektionskamera M18 SIC36-0",
-      batteryStatus: "100%",
-      temperature: "12°C",
-      updateInterval: "6 timer"
-    }
-  ];  
+  const [trackerInformation, setTrackerInformation] = useState([
+  ]);
+
+  useEffect(() => {
+    const fetchTrackerInformation = async () => {
+      console.log("Fetching tracker information...");
+      try {
+        const token = process.env.REACT_APP_TTN_API_TOKEN;
+        const appID = process.env.REACT_APP_APP_ID;
+        if (!token) {
+          console.error("API token ID is not available.");
+          return;
+        }
+        if (!appID) {
+          console.error("Application ID is not available.");
+          return;
+        }
+        const response = await fetch(`https://eu1.cloud.thethings.network/api/v3/applications/${appID}/devices?field_mask=name,description`, {
+          method: "GET",
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+  
+        if (response.ok) {
+          const data = await response.json();
+          console.log(data);
+          
+          if (data && data.end_devices) {
+            setTrackerInformation(prevState => {
+              // Iterate over the fetched data
+              data.end_devices.forEach(newDevice => {
+                const deviceIdWithoutPrefix = newDevice.ids.device_id.replace("eui-", "");
+                // Check if the device ID already exists in the current state
+                const existingIndex = prevState.findIndex(existingDevice => existingDevice.deviceId === deviceIdWithoutPrefix);
+                if (existingIndex !== -1) {
+                  // Update existing entry
+                  prevState[existingIndex] = {
+                    ...prevState[existingIndex],
+                    name: newDevice.name || "Unknown",
+                    batteryStatus: "-",
+                    temperature: "-",
+                    updateInterval: "-"
+                  };
+                } else {
+                  // Add new entry
+                  prevState.push({
+                    deviceId: deviceIdWithoutPrefix,
+                    name: newDevice.name || "Unknown",
+                    batteryStatus: "-",
+                    temperature: "-",
+                    updateInterval: "-"
+                  });
+                }
+              });
+              return [...prevState]; // Return a new array to trigger state update
+            });
+          }
+        } else {
+          console.error("Failed to fetch tracker information:", response.status, response.statusText);
+        }
+      } catch (error) {
+        console.error("Error fetching tracker information:", error);
+      }
+    };
+  
+    fetchTrackerInformation();
+  }, []);
 
   const center = [56.234538, 10.231792]; // Denmark coordinates
   const [showCurrentLocation, setShowCurrentLocation] = useState(trackerInformation.map(tracker => tracker.deviceId));
@@ -190,6 +228,10 @@ export default function App() {
   // Filter out duplicates
   const filteredAllMarkers = filterUniqueMarkers(allMarkers);
 
+  const handleTrackerInformationUpdate = (updatedTrackerInformation) => {
+    setTrackerInformation(updatedTrackerInformation);
+  };
+
   return (
     <div className="map-container">
       <button
@@ -209,7 +251,14 @@ export default function App() {
         allMarkers={filteredAllMarkers}
         useCustomClusterIcon={false}
       />
-      <SettingsMenu isOpen={settingsOpen} handleClose={handleCloseSettings} trackerInformation={trackerInformation} handleShowLocationSwitch={handleShowLocationSwitch} handleShowMovement={handleShowMovementSwitch}/>
+      <SettingsMenu 
+        isOpen={settingsOpen} 
+        handleClose={handleCloseSettings} 
+        trackerInformation={trackerInformation} 
+        handleShowLocationSwitch={handleShowLocationSwitch} 
+        handleShowMovement={handleShowMovementSwitch}
+        handleTrackerInformationUpdate={handleTrackerInformationUpdate}
+      />
     </div>
   );
 }
--- web_application/src/components/SettingsMenu.js
+++ web_application/src/components/SettingsMenu.js
@@ -2,9 +2,14 @@ import React, { useState, useEffect } from "react";
 import "./SettingsMenu.css";
 import Switch from '@mui/material/Switch';
 
-const SettingsMenu = ({ isOpen, handleClose, trackerInformation, handleShowLocationSwitch, handleShowMovement }) => {
+const SettingsMenu = ({ isOpen, handleClose, trackerInformation, handleShowLocationSwitch, handleShowMovement, handleTrackerInformationUpdate }) => {
   const [showCurrentLocationIds, setShowCurrentLocationIds] = useState(trackerInformation.map(tracker => tracker.deviceId));
   const [showMovementIds, setShowMovementIds] = useState([]);
+  const [originalNames, setOriginalNames] = useState([]);
+
+  useEffect(() => {
+    setOriginalNames(trackerInformation.map(tracker => tracker.name));
+  }, [isOpen]);
 
   const handleLocationSwitch = (id) => {
     const newShowCurrentLocationIds = showCurrentLocationIds.includes(id)
@@ -22,10 +27,135 @@ const SettingsMenu = ({ isOpen, handleClose, trackerInformation, handleShowLocat
     handleShowMovement(newShowMovementIds);
   };
 
+  const handleNameChange = (id, newName) => {
+    const updatedTrackerInformation = trackerInformation.map(tracker => {
+      if (tracker.deviceId === id) {
+        return { ...tracker, name: newName };
+      }
+      return tracker;
+    });
+    // Update tracker information with the new name
+    handleTrackerInformationUpdate(updatedTrackerInformation);
+  };
+
+  const handleCloseAndUpdateDevices = async () => {
+    // compare the original names with the updated names and print out the ones that have changed
+    const devicesToUpdate = trackerInformation.filter((tracker, index) => tracker.name !== originalNames[index]);
+    console.log("Devices to update:", devicesToUpdate);
+
+    // If there are devices to update, make API calls to update each device's name
+    if (devicesToUpdate.length === 0) {
+      console.log("No devices to update.");
+      handleClose();
+      return;
+    }
+
+    // Make API calls to update each device's name
+    const token = process.env.REACT_APP_TTN_API_TOKEN;
+    const appID = process.env.REACT_APP_APP_ID;
+    if (!token) {
+      console.error("API token ID is not available.");
+      return;
+    }
+    if (!appID) {
+      console.error("Application ID is not available.");
+      return;
+    }
+    devicesToUpdate.forEach(async (tracker) => {
+      try {
+        console.log(`Updating device ${tracker.deviceId} with original name ${originalNames[trackerInformation.findIndex(trackerInfo => trackerInfo.deviceId === tracker.deviceId)]} to new name ${tracker.name}...`);
+        const response = await fetch(`https://eu1.cloud.thethings.network/api/v3/applications/${appID}/devices/eui-${tracker.deviceId}`, {
+          method: 'PUT',
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${token}`,
+          },
+          body: JSON.stringify({
+            end_device: {
+              name: tracker.name
+            },
+            field_mask: {
+              paths: ['name']
+            }
+          })
+        });
+
+        if (!response.ok) {
+          throw new Error(`Failed to update device ${tracker.deviceId}. ${response.status}, ${response.statusText}`);
+        }
+
+        console.log(`Device ${tracker.deviceId} updated successfully`);
+      } catch (error) {
+        console.error(`Error updating device ${tracker.deviceId}:`, error);
+      }
+    });
+
+
+
+
+    setOriginalNames(trackerInformation.map(tracker => tracker.name));
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // // Iterate over trackerInformation to find devices with changed names
+    // const devicesToUpdate = trackerInformation.filter(tracker => tracker.name !== originalNames);
+    
+    // // Make API calls to update each device's name
+    // devicesToUpdate.forEach(async (tracker) => {
+    //   try {
+    //     const token = process.env.REACT_APP_TTN_API_TOKEN;
+    //     if (!token) {
+    //       console.error("API token is not available.");
+    //       return;
+    //     }
+    //     console.log(`Updating device ${tracker.deviceId}...`);
+    //     console.log(`New name: ${tracker.name}`);
+    //     console.log(`Original name: ${tracker.originalName}`);
+    //     console.log(`Application ID: ${tracker.applicationId}`);
+    //     const response = await fetch(`/api/v3/applications/${tracker.applicationId}/devices/${tracker.deviceId}`, {
+    //       method: 'PUT',
+    //       headers: {
+    //         'Content-Type': 'application/json',
+    //         Authorization: `Bearer ${token}`,
+    //       },
+    //       body: JSON.stringify({
+    //         end_device: {
+    //           name: tracker.name
+    //         },
+    //         field_mask: {
+    //           paths: ['name']
+    //         }
+    //       })
+    //     });
+
+    //     if (!response.ok) {
+    //       throw new Error(`Failed to update device ${tracker.deviceId}.`);
+    //     }
+
+    //     console.log(`Device ${tracker.deviceId} updated successfully`);
+    //   } catch (error) {
+    //     console.error(`Error updating device ${tracker.deviceId}:`, error);
+    //   }
+    // });
+
+    // Close the menu
+    handleClose();
+  };
+
   return (
     <div className={`modal ${isOpen ? "show" : ""}`}>
       <div className="modal-content">
-        <span className="close" onClick={handleClose}>&times;</span>
+        <span className="close" onClick={handleCloseAndUpdateDevices}>&times;</span>
         <table>
           <thead>
             <tr>
@@ -42,7 +172,15 @@ const SettingsMenu = ({ isOpen, handleClose, trackerInformation, handleShowLocat
             {trackerInformation.map((tracker, index) => (
               <tr key={tracker.deviceId} style={{ backgroundColor: index % 2 === 0 ? "#ffffff" : "#f9f9f9" }}>
                 <td className="id-column">{tracker.deviceId}</td>
-                <td className="name-column">{tracker.name}</td>
+                <td className="name-column">
+                  {/* Editable name field */}
+                  <input
+                    type="text"
+                    className="editable-input"
+                    value={tracker.name}
+                    onChange={(e) => handleNameChange(tracker.deviceId, e.target.value)}
+                  />
+                </td>
                 <td className="battery-status-column">{tracker.batteryStatus}</td>
                 <td className="temperature-status-column">{tracker.temperature}</td>
                 <td className="update-interval-column">{tracker.updateInterval}</td>
