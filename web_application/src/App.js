337538af20d545342fefcb18673643376562d630
promillen <s223573@dtu.dk>
2024-06-12 12:18:08 +0200

    Refactored app.js

--- web_application/src/App.js
+++ web_application/src/App.js
@@ -1,126 +1,99 @@
 import { useState, useEffect, useRef } from "react";
 import axios from "axios";
-
-import "./styles.css";
-import "leaflet/dist/leaflet.css";
-
 import MapComponent from "./components/MapComponent";
 import SettingsMenu from "./components/SettingsMenu";
 import AddDeviceMenu from "./components/AddDeviceMenu";
 import MapTileMenu from "./components/MapTileMenu";
-
 import ListIcon from '@mui/icons-material/List';
 import AddIcon from '@mui/icons-material/Add';
 import LayersIcon from '@mui/icons-material/Layers';
+import "./styles.css";
+import "leaflet/dist/leaflet.css";
+
+
+
+
 
 export default function App() {
+  // State
   const [markers, setMarkers] = useState([]);
   const [trackerInformation, setTrackerInformation] = useState([]);
-
-  useEffect(() => {
-    const fetchData = async () => {
-        await fetchTrackerInformation();
-    };
-    fetchData();
-  }, []);
-
-  const center = [56.234538, 10.231792]; // Denmark coordinates
-  const zoomLevel = 7;
-  const drawMarkersAtZoomLevel = 16;
-
   const [showCurrentLocation, setShowCurrentLocation] = useState([]);
   const [showMovement, setShowMovement] = useState([]);
   const [selectedMapTile, setSelectedMapTile] = useState("CartoDB_Voyager");
-
   const [isSettingsMenuOpen, setSettingsMenuOpen] = useState(false);
   const [isAddDeviceMenuOpen, setAddDeviceMenuOpen] = useState(false);
   const [isMapTileMenuOpen, setMapTileMenuOpen] = useState(false);
+  const [dataLoaded, setDataLoaded] = useState(false);
+
+  // Refs
   const settingsMenuRef = useRef(null);
   const addDeviceMenuRef = useRef(null);
   const mapTileMenuRef = useRef(null);
 
-  const [dataLoaded, setDataLoaded] = useState(false);
+  // Constants
+  const center = [56.234538, 10.231792]; // Denmark coordinates
+  const zoomLevel = 7;
+  const drawMarkersAtZoomLevel = 16;
+
+  // Effects
   useEffect(() => {
     console.log("Version 12/06/2023 - 12:00");
   }, []);
-  // Function to handle setting showCurrentLocation once all data is loaded
+
   useEffect(() => {
-    // Check if both markers and tracker information have been fetched
+    const fetchData = async () => {
+      await fetchTrackerInformation();
+    };
+    fetchData();
+  }, []);
+
+  useEffect(() => {
     if (markers.length > 0 && trackerInformation.length > 0 && !dataLoaded) {
-      // Extract all IDs from trackerInformation
       const allIds = trackerInformation.map(info => info.deviceId.toUpperCase());
-      // Set showCurrentLocation to contain all IDs
       setShowCurrentLocation(allIds);
-      // Update dataLoaded state to prevent re-execution of this useEffect
       setDataLoaded(true);
     }
   }, [markers, trackerInformation, dataLoaded]);
 
-  const handleOpenSettingsMenu = () => {
-    setSettingsMenuOpen(true);
-  };
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (settingsMenuRef.current && !settingsMenuRef.current.contains(event.target)) {
+        setSettingsMenuOpen(false);
+      }
+      if (addDeviceMenuRef.current && !addDeviceMenuRef.current.contains(event.target)) {
+        setAddDeviceMenuOpen(false);
+      }
+      if (mapTileMenuRef.current && !mapTileMenuRef.current.contains(event.target)) {
+        setMapTileMenuOpen(false);
+      }
+    };
 
-  const handleCloseSettingsMenu = () => {
-    setSettingsMenuOpen(false);
-  };
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
 
-  const handleOpenAddDeviceMenu = () => {
-    setAddDeviceMenuOpen(true);
-  };
-
-  const handleCloseAddDeviceMenu = () => {
-    setAddDeviceMenuOpen(false);
-  };
-
-  const handleOpenMapTileMenu = () => {
-    setMapTileMenuOpen(true);
-  };
-
-  const handleCloseMapTileMenu = () => {
-    setMapTileMenuOpen(false);
-  };
-
+  // Handlers
+  const handleOpenSettingsMenu = () => setSettingsMenuOpen(true);
+  const handleCloseSettingsMenu = () => setSettingsMenuOpen(false);
+  const handleOpenAddDeviceMenu = () => setAddDeviceMenuOpen(true);
+  const handleCloseAddDeviceMenu = () => setAddDeviceMenuOpen(false);
+  const handleOpenMapTileMenu = () => setMapTileMenuOpen(true);
+  const handleCloseMapTileMenu = () => setMapTileMenuOpen(false);
+  const handleShowLocationSwitch = (ids) => setShowCurrentLocation(ids.map(id => id.toUpperCase()));
+  const handleShowMovementSwitch = (ids) => setShowMovement(ids.map(id => id.toUpperCase()));
   const handleTileChange = (tile) => {
     setSelectedMapTile(tile);
     console.log(`Map tile changed to: ${tile}`);
   };
-
-  const handleShowLocationSwitch = (ids) => {
-    // Convert all IDs to lowercase before setting the state
-    const uppercaseIds = ids.map(id => id.toUpperCase());
-    setShowCurrentLocation(uppercaseIds);
+  const handleTrackerInformationUpdate = (updatedTrackerInformation) => {
+    setTrackerInformation(updatedTrackerInformation);
   };
 
-  const handleShowMovementSwitch = (ids) => {
-    // Convert all IDs to lowercase before setting the state
-    const uppercaseIds = ids.map(id => id.toUpperCase());
-    setShowMovement(uppercaseIds);
-  };
-
-  // Function to handle successful addition of device and refetch tracker information
-  const handleAddDeviceSuccess = async () => {
-    try {
-      // Fetch tracker information again
-      await fetchTrackerInformation();
-    } catch (error) {
-      console.error("Error fetching tracker information:", error);
-      // Handle error fetching tracker information
-    }
-  };
-
-  const getDeviceInfo = async () => {
-    try {
-      const response = await axios.get("/api/getDeviceInfoFromJoinserver");
-      console.log(response.data);
-      // Process device information as needed
-    } catch (error) {
-      console.error("Failed to fetch device information:", error);
-      alert("Failed to fetch device information. Check console for details.");
-    }
-  };
-
-  // Function to filter the markers so that only the newest marker for each ID is included
-  const filterNewestMarkers = (markers) => {
+  // Helper functions
+  const filterNewestMarkers = (markers) => {  // Function to filter the markers so that only the newest marker for each ID is included
     const newestMarkers = {};
     markers.forEach(marker => {
       if (!newestMarkers[marker.deviceId] || marker.timeStamp > newestMarkers[marker.deviceId].timeStamp) {
@@ -130,18 +103,7 @@ export default function App() {
     return Object.values(newestMarkers);
   };
 
-  // Update filtered markers when toggling switches
-  const filteredCurrentLocationMarkers = filterNewestMarkers(markers.filter(marker => showCurrentLocation.includes(marker.deviceId)));
-  const filteredAllLocationMarkers = markers.filter(marker => showMovement.includes(marker.deviceId));
-
-  // Combine all markers
-  const allMarkers = [
-    ...filteredCurrentLocationMarkers, 
-    ...filteredAllLocationMarkers
-];
-
-  // Function to filter out duplicates from all markers
-  const filterUniqueMarkers = (markers) => {
+  const filterUniqueMarkers = (markers) => { // Function to filter out duplicates from all markers
     const uniqueMarkers = {};
     markers.forEach(marker => {
       if (!uniqueMarkers[marker.pingId]) {
@@ -151,13 +113,7 @@ export default function App() {
     return Object.values(uniqueMarkers);
   };
 
-  // Filter out duplicates
-  const filteredAllMarkers = filterUniqueMarkers(allMarkers);
-
-  const handleTrackerInformationUpdate = (updatedTrackerInformation) => {
-    setTrackerInformation(updatedTrackerInformation);
-  };
-
+  // Fetch functions
   const fetchMarkers = async () => {
     console.log("Fetching markers...");
     try {
@@ -268,25 +224,33 @@ export default function App() {
     }
   };
 
-   // Close menu when clicking outside
-  useEffect(() => {
-    const handleClickOutside = (event) => {
-      if (settingsMenuRef.current && !settingsMenuRef.current.contains(event.target)) {
-        setSettingsMenuOpen(false);
-      }
-      if (addDeviceMenuRef.current && !addDeviceMenuRef.current.contains(event.target)) {
-        setAddDeviceMenuOpen(false);
-      }
-      if (mapTileMenuRef.current && !mapTileMenuRef.current.contains(event.target)) {
-        setMapTileMenuOpen(false);
-      }
-    };
+  const getDeviceInfo = async () => {
+    try {
+      const response = await axios.get("/api/getDeviceInfoFromJoinserver");
+      console.log(response.data);
+      // Process device information as needed
+    } catch (error) {
+      console.error("Failed to fetch device information:", error);
+      alert("Failed to fetch device information. Check console for details.");
+    }
+  };
 
-    document.addEventListener("mousedown", handleClickOutside);
-    return () => {
-      document.removeEventListener("mousedown", handleClickOutside);
-    };
-  }, []);
+  // Function to handle successful addition of device and refetch tracker information
+  const handleAddDeviceSuccess = async () => {
+    try {
+      // Fetch tracker information again
+      await fetchTrackerInformation();
+    } catch (error) {
+      console.error("Error fetching tracker information:", error);
+      // Handle error fetching tracker information
+    }
+  };
+
+  // Derived data
+  const filteredCurrentLocationMarkers = filterNewestMarkers(markers.filter(marker => showCurrentLocation.includes(marker.deviceId))); // Update filtered markers when toggling switches
+  const filteredAllLocationMarkers = markers.filter(marker => showMovement.includes(marker.deviceId)); // Update filtered markers when toggling switches
+  const allMarkers = [...filteredCurrentLocationMarkers, ...filteredAllLocationMarkers]; // Combine all markers
+  const filteredAllMarkers = filterUniqueMarkers(allMarkers); // Filter out duplicates  
 
   return (
     <div className="map-container">
